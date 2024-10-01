b70deea39b6ece80e91fe4f9b5ebe9020225b5b8
mahmut-a <xmahmut@proton.me>
2024-10-01 20:29:41 +0300

    Vercel - 7.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
-import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
