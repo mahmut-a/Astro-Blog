cd116c7a2082afb2c119b35e05be24305c12ce2f
mahmut-a <xmahmut@proton.me>
2024-10-01 20:26:15 +0300

    Vercel - 7

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
-import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
