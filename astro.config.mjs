317e39c3c684518d0bb7522d92c42d5c502fbbfb
mahmut-a <xmahmut@proton.me>
2024-10-01 20:18:42 +0300

    Vercel - 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from 'astro/config';
 
 import vercel from '@astrojs/vercel/serverless';
 
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel({
-    webAnalytics: { enabled: true }
+    webAnalytics: {
+      enabled: true,
+    },
   }),
 });
-
-// https://astro.build/config
-export default defineConfig({});
